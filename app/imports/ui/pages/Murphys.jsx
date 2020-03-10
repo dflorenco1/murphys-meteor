39b525274e194ff303b06e7c92843c6b8b5cbc8c
dflorenco1 <60244325+dflorenco1@users.noreply.github.com>
2020-03-10 11:35:29 -1000

    intial

---
+++ app/imports/ui/pages/Murphys.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Middle from '../components/Middle';
+
+
+export default class Murphys extends React.Component {
+
+  render() {
+    return (
+        <div>
+          <Middle/>
+        </div>
+    );
+  }
+}
